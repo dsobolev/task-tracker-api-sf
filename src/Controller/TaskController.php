a41ca9fdc2864367125b195528f3c3c7abc21119
dsobolev <denis.sobolev1@gmail.com>
2024-07-01 12:37:37 +0100

    Added create-task method

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Entity\TaskStatus;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
-    public function __construct(private TaskRepository $taskRepo) { }
+    public function __construct(
+        private TaskRepository $taskRepo,
+        private EntityManagerInterface $em
+    ) { }
 
     #[Route('/tasks', name: 'tasks_list')]
     public function index(): JsonResponse
@@ -40,5 +48,30 @@ class TaskController extends AbstractController
         ];
 
         return $this->json(['taskData' => $result]);
+    }
+
+    #[Route('task/new'), name: 'new_task']
+    public function create(Request $request): JsonResponse
+    {
+        $title = $request->get('title');
+        if (empty($title)) {
+            return $this->json(['msg' => 'Title field required'], Response::HTTP_BAD_REQUEST);
+        }
+        $task = new Task();
+        $task->setTitle($title);
+
+        $description = $request->get('description');
+        if (!empty($description)) {
+            $task->setDescription($description);
+        }
+
+        // Default status is ToDo
+        $task->setStatus(TaskStatus::ToDo);
+
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+        return $this->json(['id' => '']);
     }
 }
